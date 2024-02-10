e61fd2d3502b28254cf11b8b4709a9cb06ef9676
Punkin199573 <143465572+Punkin199573@users.noreply.github.com>
2024-02-10 15:42:32 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/img_0980.png'}
-        <h1 style={{ color: 'black' }}>WELCOME TO FEMONIC BRIDGEPOINT FINANCIAL SERVICE</h1>
+        title={ style={{ color: 'black' }} WELCOME TO FEMONIC BRIDGEPOINT FINANCIAL SERVICE}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
         ctaAction={goToShop}
