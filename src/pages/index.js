38e08f9f64825de123ac4b333c6d192950b46021
Punkin199573 <143465572+Punkin199573@users.noreply.github.com>
2024-02-11 14:00:38 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
         maxWidth={'500px'}
         image={'/img_0980.png'}
         title={ style={{ color: 'black' }} WELCOME TO FEMONIC BRIDGEPOINT FINANCIAL SERVICE}
-        subtitle={'Discover Autumn Winter 2021'}
+        subtitle={'WELCOME TO FEMONIC BRIDGEPOINT FINANCIAL SERVICES'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
